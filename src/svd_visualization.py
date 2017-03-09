b45b3a4c895c4548a28a914c59a388d20cdb5031
Veb Anand <Veb@Vebs-MacBook-Pro.local>
2017-03-08 23:32:30 -0800

    more comparison and 2d visualization

--- src/svd_visualization.py
+++ src/svd_visualization.py
@@ -61,6 +61,34 @@ def twoD_viz(title, movie_ids, xy_coords, rect, dpi=100, buf=0.1, annotate=True)
                transparent=True, dpi=dpi)
 
 
+def viz_comparison(title, xy_coords_lst, colors, rect, dpi=100):
+    # create plot dimensions, resolution, and axis ranges
+    plt.clf()
+    fig, ax = plt.subplots(figsize=(10., 10.), dpi=dpi)
+    ax.set_title(title, fontdict={'fontsize': 25})
+    ax.axis(rect)
+
+    # get relative sizes and ranges of drawing window
+    x_mid, y_mid = (rect[0] + rect[1]) / 2., (rect[2] + rect[3]) / 2.
+    x_interval, y_interval = (rect[1] - rect[0]), (rect[3] - rect[2])
+    
+    # draw points. use decimal colors if one group. else, colors are distinct
+    if len(xy_coords_lst) == 1:
+        ax.scatter(xy_coords_lst[0][:, 0], xy_coords_lst[0][:, 1], c=colors[0])
+    else:
+        for i, xy_coords in enumerate(xy_coords_lst):
+            ax.scatter(xy_coords[:, 0], xy_coords[:, 1], color=colors[i])
+    
+    # remove top and right axis-lines for aesthetic purposes
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    # save plot
+    print('saving...', title)
+    plt.savefig('../graphics/{0}.png'.format(title), bbox_inches='tight', 
+               transparent=True, dpi=dpi)
+
+
 def viz_tasks(V):
     '''
     param: V: 2-d numpy matrix as defined in guide as "V", V.shape = [2, n],
@@ -78,26 +106,53 @@ def viz_tasks(V):
     movie_avg_ratings = []
     for movie, ratings in movie_ratings:
         movie_sizes.append(len(ratings))
-        movie_avg_ratings.append(sum(ratings) / len(ratings))    
-    most_popular = np.array(list(zip(*sorted(zip(movie_sizes, movie_ids))))[1][-10:])
+        movie_avg_ratings.append(sum(ratings) / len(ratings))
+    all_popular = np.array(list(zip(*sorted(zip(movie_sizes, movie_ids)))))
+    most_popular = all_popular[1][-10:]
 
     # 10 best movies
-    best_movies = np.array(list(zip(*sorted(zip(movie_avg_ratings, movie_ids))))[1][-10:])
+    best_movies = np.array(list(zip(*sorted(zip(movie_avg_ratings, movie_ids)))))[1][-10:]
 
-    # first 10 movies from three genres
-    animation, scifi, war = [np.array([x for x, y in genre][:10])
-        for genre in [import_genres(['Animation']), import_genres(['Sci-Fi']), 
-        import_genres(['War'])]]
+    # first 10 movies from three genres. only include over 50 ratings
+    fantasy_all = import_genres(['Fantasy'])
+    scifi_all = import_genres(['Sci-Fi'])
+    war_all = import_genres(['War'])
+    fantasy, scifi, war = [np.array([x for x, y in 
+        sorted(genre, key=lambda x: sum(x[1]) / len(x[1]), reverse=True) if 
+        len(y) >= 50][:10]) for genre in [fantasy_all, scifi_all, war_all]]
+    # regroup models. only include over 50 ratings
+    fantasy_coords = [V[np.array([x for x, y in genre if len(y) >= 50]) - 1] 
+        for genre in [fantasy_all, scifi_all, war_all]]
+
+    # turn popularity values into colors
+    all_popular = all_popular.astype(np.float32)
+    all_popular[0] /= all_popular[0].max()
+    all_popular[0] = all_popular[0] ** .25
+
+    # get rating values above 20 frequency and turn into colors
+    all_best = [x for x in movie_ratings if len(x[1]) >= 20]
+    all_best = [[x[0], sum(x[1]) / len(x[1])] for x in all_best]
+    all_best = sorted(all_best, key=lambda x: x[1], reverse=True)
+    all_best = np.array(all_best).astype(np.float32).transpose()
+    all_best[1] /= all_best[1].max()
+    all_best[1] = all_best[1]
 
     # rectangle bounding all possible xy coordinates in V
     rect = [V[:, 0].min(), V[:, 0].max(), V[:, 1].min(), V[:, 1].max()]
 
     # create and save plots using movie ids and coordinates from V
     twoD_viz('All Movies', movie_ids, V, rect, annotate=False)
+    viz_comparison('Genre Comparison', fantasy_coords, 
+        ['blue', 'green', 'red'], rect)
+    viz_comparison('Popularity Comparison', 
+        [V[all_popular[1].astype(np.uint16) - 1]], [all_popular[0]], rect)
+    viz_comparison('Rating Comparison', 
+        [V[all_best[0].astype(np.uint16) - 1]], [all_best[1]], rect)
+    quit()
     twoD_viz('10 Random Films', rand_movies, V[rand_movies - 1], rect)
     twoD_viz('10 Most Popular Films', most_popular, V[most_popular - 1], rect)
-    twoD_viz('10 Most Rated Films', best_movies, V[best_movies - 1], rect)
-    twoD_viz('Top 10 Genre: Animation Films', animation, V[animation - 1], rect)
+    twoD_viz('10 Top Rated Films', best_movies, V[best_movies - 1], rect)
+    twoD_viz('Top 10 Genre: Fantasy Films', fantasy, V[fantasy - 1], rect)
     twoD_viz('Top 10 Genre: Sci-Fi Films', scifi, V[scifi - 1], rect)
     twoD_viz('Top 10 Genre: War Films', war, V[war - 1], rect)
     print('done!')
